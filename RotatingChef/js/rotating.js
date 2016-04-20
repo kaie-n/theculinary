e7c87b393de66b2bff28893216bb71b71c6ffb66
Abdul Qayyum <kaien@alyssaa.net>
2016-04-20 16:00:38 +0800

    test

--- RotatingChef/js/rotating.js
+++ RotatingChef/js/rotating.js
@@ -1,23 +1,40 @@
-﻿var details = [0, 7, 14, 26];
+﻿// Details variable for four sides of angles
+var details = [0, 7, 14, 26];
 var detailsInfo;
-// the current index in the details array
 var detailIndex = 0;
+
+var detailMale;
+var detailFemale;
+
+// Variables for 360 Spin Frameworks
 var api;
 var space = 150;
 var frames;
+var framesFemale
 var spin;
 var slide;
+
+// Booleans
+var runOnce = false;
+
+// First image
 var image;
 var imageSize = {
     "width": 0,
     "height": 0
 }
-// use helper method to generate an array of image urls. We have 34 frames in total
+
+// Frames for male and female pictures
 frames = SpriteSpin.sourceArray('img/male/chef_male_{frame}.jpg', {
     frame: [1, 32],
     digits: 2
 });
-// set image size width and height;
+framesFemale = SpriteSpin.sourceArray('img/female/chef_female_{frame}.jpg', {
+    frame: [1, 32],
+    digits: 2
+});
+
+// Set image size width and height for imagemaps;
 var img = new Image();
 img.onload = function () {
     imageSize.width = this.width;
@@ -25,30 +42,41 @@ img.onload = function () {
     createImage();
 };
 img.src = frames[0];
-window.onload = function () {
-    $('#slider').slider();
 
-    $.getJSON("js/details.json", function (result) {
-        $.each(result, function (i, field) {
-            detailsInfo = result.details;
-
-        });
-
+// Preloading the images
+function preload(arrayOfImages) {
+    $(arrayOfImages).each(function () {
+        $('<img />').attr('src', this).appendTo('body').css('display', 'none');
     });
-    spin = $('#spinner');
-    slide = $('#slider');
+}
+preload(frames);
 
-    //createImage();
-    console.log(imageSize.width);
+// Loading the JSON containing the information (details.json)
+$.getJSON("js/details.json", function (result) {
+    $.each(result, function (i, field) {
+        detailsInfo = result.details;
+    });
+});
 
+
+// Create the spinner
+function createSpinner(framesX, boolX, gender) {
+    createImage();
+
+    spin = $('#spinner-male');
     spin.spritespin({
         // path to the source images.
-        source: frames,
+        source: framesX,
         width: 259,  // width in pixels of the window/frame
         height: 551,  // height in pixels of the window/frame
         animate: false,
-        wrap: false,
+        wrap: true,
+        behaviour: '360',
+        reverse: false,
         onLoad: function () {
+            if (boolX) {
+                $('#overlay').fadeOut();
+            }
             slide.slider({
                 min: 0,
                 max: frames.length - 1,
@@ -69,32 +97,30 @@ window.onload = function () {
 
     spin.bind("onLoad", function () {
         var data = api.data;
-        data.stage.prepend($(".details .detail")); // add current details
+        //var string = $(".details .detail").clone();
+        if (gender == "male") {
+            data.stage.prepend(detailMale); // add current details
+        }
+        if (gender == "female") {
+            data.stage.prepend(detailFemale); // add current details
+        }
         //data.stage.prepend($("#Map0"));
         data.stage.find(".detail").hide();         // hide current details
+        createImageMapster();
     }).bind("onFrame", function () {
         var data = api.data;
         data.stage.find(".detail:visible").stop(false).fadeOut();
         data.stage.find(".detail.detail-" + data.frame).stop(false).fadeIn();
     });
+}
 
-    $("#btn-prev").click(function () {
-        detailIndex--
-        setDetailIndex(detailIndex);
-    });
 
-    $("#btn-next").click(function () {
-        detailIndex++
-        setDetailIndex(detailIndex);
-    });
-    $("#btn-home").click(function () {
-        $('#home').show();
-        $('.div-detail').addClass('hide').removeClass('show');
-    })
+// Initiation the image maps for specific parts of the images
+function createImageMapster() {
     image = $('img');
     image.mapster({
-        fillColor: 'CCCCCC',
-        fillOpacity: 0.3,
+        fillColor: '6c6c6c',
+        fillOpacity: 0.5,
         stroke: true,
         strokeColor: "000000",
         strokeOpacity: 0.8,
@@ -111,15 +137,34 @@ window.onload = function () {
             $('.div-detail').addClass('hide').removeClass('show');
             $(id).addClass('show').removeClass('hide');
 
-
-
             $(id).html('<h1>' + title + '</h1><p>' + detailsInfo[selector] + '</p>');
             // update text depending on area selected
             // if Asparagus selected, change the tooltip
 
         }
     });
-    createDivs();
+    if (!runOnce) {
+        runOnce = true;
+        createDivs();
+    }
+    $("#show-all").hover(function () {
+
+        $('area').mapster('select');
+    },
+   function () {
+       $('area').mapster('deselect');
+   });
+}
+
+function createNavigation() {
+    var a = $('#navigational');
+    var c = 1;
+
+    for (var i = 0; i < details.length; i++) {
+        var b = '<li><a href="#" onclick="setDetailIndex(' + i + ')">' + c + '</a></li>';
+        a.append(b);
+        c++;
+    }
 }
 
 function createDivs() {
@@ -132,30 +177,19 @@ function createDivs() {
         console.log($(this).attr('data-key'), i, title);
         i++;
     });
-
-
     $('#information').append($('#nav'));
 }
-
-$(window).resize(function () {
-    //$('#spinner').width(Math.round((window.innerHeight - space) * (imageSize.width / imageSize.height)))
-    //$('#spinner canvas').width(Math.round((window.innerHeight - space) * (imageSize.width / imageSize.height)))
-    //$('#spinner').height(Math.round(window.innerHeight - space))
-    //$('#spinner canvas').height(Math.round(window.innerHeight - space))
-
-});
 
 function createImage() {
     for (var i = 0; i < details.length; i++) {
         var n = details[i];
-        if (i == 0) {
-            $('.details').append('<div class="detail detail-' + n + '"><img src="' + frames[n] + '"  usemap="#Map0"/></div>')
-        }
-        else {
-            $('.details').append('<div class="detail detail-' + n + '"><img src="' + frames[n] + '" /></div>')
-        }
+            $('.details-male').append('<div class="detail detail-' + n + '"><img src="' + frames[n] + '"  usemap="#Map'+ n +'"/></div>')
+        //else {
+        //    $('.details-male').append('<div class="detail detail-' + n + '"><img src="' + frames[n] + '" /></div>')
+        //}
     }
-    $('.detail-0').append($('#Map0'))
+    var map = $('#Map0')
+    $('.detail-0').append(map)
 }
 
 function setDetailIndex(index) {
@@ -170,3 +204,34 @@ function setDetailIndex(index) {
 
     api.playTo(details[detailIndex]);
 }
+
+// Run this once DOM element is loaded
+window.onload = function () {
+    detailMale = $(".details-male .detail").clone();
+    $(".details-male .detail").remove();
+    $('#slider').slider();
+
+
+    spin = $('#spinner-male');
+    slide = $('#slider');
+
+    //createImage();
+    //console.log(imageSize.width);
+
+    createSpinner(frames, true, "male")
+
+    $("#btn-home").click(function () {
+        $('#home').show();
+        $('.div-detail').addClass('hide').removeClass('show');
+    })
+    $("#btn-female").click(function () {
+        createSpinner(framesFemale, false, "female")
+    })
+    $("#btn-male").click(function () {
+        createSpinner(frames, false, "male")
+    })
+    createNavigation();
+}
+
+
+
