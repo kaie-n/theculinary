3c69c879732355ed5fb4466a989b44215144ab79
Abdul Qayyum <kaien@alyssaa.net>
2016-06-27 11:40:43 +0800

    test

---
+++ RotatingChef/storyline/test-projectv1 - Storyline output/js/rotating.js
@@ -0,0 +1,237 @@
+﻿// Details variable for four sides of angles
+var details = [0, 7, 14, 26];
+var detailsInfo;
+var detailIndex = 0;
+
+var detailMale;
+var detailFemale;
+
+// Variables for 360 Spin Frameworks
+var api;
+var space = 150;
+var frames;
+var framesFemale
+var spin;
+var slide;
+
+// Booleans
+var runOnce = false;
+
+// First image
+var image;
+var imageSize = {
+    "width": 0,
+    "height": 0
+}
+
+// Frames for male and female pictures
+frames = SpriteSpin.sourceArray('img/male/chef_male_{frame}.jpg', {
+    frame: [1, 32],
+    digits: 2
+});
+framesFemale = SpriteSpin.sourceArray('img/female/chef_female_{frame}.jpg', {
+    frame: [1, 32],
+    digits: 2
+});
+
+// Set image size width and height for imagemaps;
+var img = new Image();
+img.onload = function () {
+    imageSize.width = this.width;
+    imageSize.height = this.height;
+    createImage();
+};
+img.src = frames[0];
+
+// Preloading the images
+function preload(arrayOfImages) {
+    $(arrayOfImages).each(function () {
+        $('<img />').attr('src', this).appendTo('body').css('display', 'none');
+    });
+}
+preload(frames);
+
+// Loading the JSON containing the information (details.json)
+$.getJSON("js/details.json", function (result) {
+    $.each(result, function (i, field) {
+        detailsInfo = result.details;
+    });
+});
+
+
+// Create the spinner
+function createSpinner(framesX, boolX, gender) {
+    createImage();
+
+    spin = $('#spinner-male');
+    spin.spritespin({
+        // path to the source images.
+        source: framesX,
+        width: 259,  // width in pixels of the window/frame
+        height: 551,  // height in pixels of the window/frame
+        animate: false,
+        wrap: true,
+        behaviour: '360',
+        reverse: false,
+        onLoad: function () {
+            if (boolX) {
+                $('#overlay').fadeOut();
+            }
+            slide.slider({
+                min: 0,
+                max: frames.length - 1,
+                slide: function (e, ui) {
+                    var api = spin.spritespin('api');
+                    api.stopAnimation();
+                    api.updateFrame(ui.value);
+                }
+            })
+        },
+        onFrame: function (e, data) {
+            slide.slider('value', data.frame);
+            $('.detail').css({ "display": "none" });
+
+        }
+    });
+    api = spin.spritespin("api");
+
+    spin.bind("onLoad", function () {
+        var data = api.data;
+        //var string = $(".details .detail").clone();
+        if (gender == "male") {
+            data.stage.prepend(detailMale); // add current details
+        }
+        if (gender == "female") {
+            data.stage.prepend(detailFemale); // add current details
+        }
+        //data.stage.prepend($("#Map0"));
+        data.stage.find(".detail").hide();         // hide current details
+        createImageMapster();
+    }).bind("onFrame", function () {
+        var data = api.data;
+        data.stage.find(".detail:visible").stop(false).fadeOut();
+        data.stage.find(".detail.detail-" + data.frame).stop(false).fadeIn();
+    });
+}
+
+
+// Initiation the image maps for specific parts of the images
+function createImageMapster() {
+    image = $('img');
+    image.mapster({
+        fillColor: '6c6c6c',
+        fillOpacity: 0.5,
+        stroke: true,
+        strokeColor: "000000",
+        strokeOpacity: 0.8,
+        mapKey: 'data-key',
+        showToolTip: true,
+        isSelectable: false,
+        onClick: function (e) {
+            console.log(e.key);
+            var id = '#' + e.key;
+            var selector = $(id).attr("data-id");
+            var title = $(id).attr("data-title");
+            selector = parseInt(selector, 10);
+            $('#home').hide();
+            $('.div-detail').addClass('hide').removeClass('show');
+            $(id).addClass('show').removeClass('hide');
+
+            $(id).html('<h1>' + title + '</h1><p>' + detailsInfo[selector] + '</p>');
+            // update text depending on area selected
+            // if Asparagus selected, change the tooltip
+
+        }
+    });
+    if (!runOnce) {
+        runOnce = true;
+        createDivs();
+    }
+    $("#show-all").hover(function () {
+
+        $('area').mapster('select');
+    },
+   function () {
+       $('area').mapster('deselect');
+   });
+}
+
+function createNavigation() {
+    var a = $('#navigational');
+    var c = 1;
+
+    for (var i = 0; i < details.length; i++) {
+        var b = '<li><a href="#" onclick="setDetailIndex(' + i + ')">' + c + '</a></li>';
+        a.append(b);
+        c++;
+    }
+}
+
+function createDivs() {
+    var d = $('area[data-key]');
+    var i = 0;
+    d.each(function () {
+        var val = $(this).attr('data-key');
+        var title = $(this).attr('data-title');
+        $('#information').append('<div class="col-sm-12 div-detail hide" data-title="' + title + '" data-id="' + i + '" id="' + val + '"></div>');
+        console.log($(this).attr('data-key'), i, title);
+        i++;
+    });
+    $('#information').append($('#nav'));
+}
+
+function createImage() {
+    for (var i = 0; i < details.length; i++) {
+        var n = details[i];
+            $('.details-male').append('<div class="detail detail-' + n + '"><img src="' + frames[n] + '"  usemap="#Map'+ n +'"/></div>')
+        //else {
+        //    $('.details-male').append('<div class="detail detail-' + n + '"><img src="' + frames[n] + '" /></div>')
+        //}
+    }
+    var map = $('#Map0')
+    $('.detail-0').append(map)
+}
+
+function setDetailIndex(index) {
+    $('.detail').css({ "display": "none" })
+    detailIndex = index;
+    if (detailIndex < 0) {
+        detailIndex = details.length - 1;
+    }
+    if (detailIndex >= details.length) {
+        detailIndex = 0;
+    }
+
+    api.playTo(details[detailIndex]);
+}
+
+// Run this once DOM element is loaded
+window.onload = function () {
+    detailMale = $(".details-male .detail").clone();
+    $(".details-male .detail").remove();
+    $('#slider').slider();
+
+
+    spin = $('#spinner-male');
+    slide = $('#slider');
+
+    //createImage();
+    //console.log(imageSize.width);
+
+    createSpinner(frames, true, "male")
+
+    $("#btn-home").click(function () {
+        $('#home').show();
+        $('.div-detail').addClass('hide').removeClass('show');
+    })
+    $("#btn-female").click(function () {
+        createSpinner(framesFemale, false, "female")
+    })
+    $("#btn-male").click(function () {
+        createSpinner(frames, false, "male")
+    })
+    createNavigation();
+}
+
+
+
---
+++ RotatingChef/storyline/test-projectv1 - Storyline output/js/rotatingv2.js
@@ -0,0 +1,263 @@
+﻿// begin variable listing
+var current;
+var male = new Character([0, 7, 14, 26], "");
+var female = new Character([0, 7, 14, 26], "");
+setFrame(male, 'img/male/chef_male_{frame}.jpg');
+setFrame(female, 'img/female/chef_female_{frame}.jpg');
+
+var detailsInfo;
+// Loading the JSON containing the information (details.json)
+$.getJSON("js/details.json", function (result) {
+    $.each(result, function (i, field) {
+        detailsInfo = result.details;
+    });
+});
+
+
+/**
+  * @desc Character class
+  * @param 'array' detail, which stores the number of specific frames to show detail in (image maps)
+*/
+function Character(detail, spin){
+    this.frames = [];
+    this.detail = detail;
+    this.spin = spin;
+    this.api = "";
+    this.index = 0;
+    this.clone = function (string) {
+        string.clone();
+        string.remove();
+        return string;
+    }
+    this.content;
+}
+
+/**
+  * @desc initialize the main core libraries and frameworks
+  * @desc hopefully it will work
+*/
+function init() {
+    male.spin = $('#spinner-male');
+    female.spin = $('#spinner-female');
+    // spritespin initialize
+    female.spin.spritespin({
+        // path to the source images.
+        source: female.frames,
+        width: 259,  // width in pixels of the window/frame
+        height: 551,  // height in pixels of the window/frame
+        animate: false,
+        wrap: true,
+        sense: 0,
+        behaviour: '360',
+        reverse: false,
+        onLoad: function () {
+
+        },
+        onFrame: function (e, data) {
+        }
+    });
+    // spritespin initialize
+    male.spin.spritespin({
+        // path to the source images.
+        source: male.frames,
+        width: 259,  // width in pixels of the window/frame
+        height: 551,  // height in pixels of the window/frame
+        animate: false,
+        wrap: true,
+        behaviour: '360',
+        sense: 0,
+        reverse: false,
+        onLoad: function () {
+            
+        },
+        onFrame: function (e, data) {
+        }
+    });
+
+    female.spin.hide();
+    male.api = male.spin.spritespin('api');
+    female.api = female.spin.spritespin('api');
+    current = male;
+    createImage('.details-male', 'male', male);
+    createImage('.details-female', 'female', female);
+
+    male.content = male.clone($(".details-male .detail"));
+    female.content = female.clone($(".details-female .detail"));
+    createNavigation($('#navigational'));
+    current.spin.bind("onLoad", function () {
+        var data = current.api.data;
+            data.stage.prepend(current.content); // add current details
+        //data.stage.prepend($("#Map0"));
+        data.stage.find(".detail").hide();         // hide current details
+        createImageMapster("img.male");
+    }).bind("onFrame", function () {
+        var data = current.api.data;
+        data.stage.find(".detail:visible").stop(false).fadeOut();
+        data.stage.find(".detail.detail-" + data.frame).stop(false).fadeIn();
+    });
+    female.spin.bind("onLoad", function () {
+        var data = female.api.data;
+        data.stage.prepend(female.content); // add current details
+        //data.stage.prepend($("#Map0"));
+        data.stage.find(".detail").hide();         // hide current details
+        createImageMapster("img.female");
+    }).bind("onFrame", function () {
+        var data = female.api.data;
+        data.stage.find(".detail:visible").stop(false).fadeOut();
+        data.stage.find(".detail.detail-" + data.frame).stop(false).fadeIn();
+    });
+}
+
+/**
+  * @desc Dynamically create the image for image mapster to work.
+  * @desc hopefully it will work though haha
+*/
+function createImage(string, gender, current) {
+    for (var i = 0; i < current.detail.length; i++) {
+        var n = current.detail[i];
+        $(string).append('<div class="detail detail-' + n + '"><img class="'+gender+'"src="' + current.frames[n] + '"  usemap="#'+ gender +'Map' + n + '"/></div>')
+        //$('.details-male').append('<div class="detail detail-' + n + '"><img src="' + current.frames[n] + '"  usemap="#maleMap' + n + '"/></div>')
+        //else {
+        //    $('.details-male').append('<div class="detail detail-' + n + '"><img src="' + frames[n] + '" /></div>')
+        //}
+    }
+    var map = $('#Map0')
+    $('.detail-0').append(map)
+}
+/**
+  * @desc Initiation the image maps for specific parts of the images
+  * @desc hopefully it will work
+*/
+function createImageMapster(string) {
+    image = $(string);
+    console.log(image);
+    image.mapster({
+        fillColor: '6c6c6c',
+        fillOpacity: 0.5,
+        stroke: true,
+        strokeColor: "000000",
+        strokeOpacity: 0.8,
+        mapKey: 'data-key',
+        showToolTip: true,
+        isSelectable: false,
+        onClick: function (e) {
+            console.log(e.key);
+            var id = '#' + e.key;
+            var selector = $(id).attr("data-id");
+            var title = $(id).attr("data-title");
+            var player = GetPlayer();
+            console.log(title);
+            player.SetVar("Layer", e.key);
+            //selector = parseInt(selector, 10);
+            //$('#home').hide();
+            //$('.div-detail').addClass('hide').removeClass('show');
+            //$(id).addClass('show').removeClass('hide');
+
+            //$(id).html('<h1>' + title + '</h1><p>' + detailsInfo[selector] + '</p>');
+            // update text depending on area selected
+            // if Asparagus selected, change the tooltip
+
+        }
+    });
+   
+        createDivs();
+    $("#show-all").hover(function () {
+
+        $('area').mapster('select');
+    },
+   function () {
+       $('area').mapster('deselect');
+   });
+}
+/**
+  * @desc create the individual details menu!
+  * @desc hopefully it will work
+*/
+function createDivs() {
+    var d = $('area[data-key]');
+    var i = 0;
+    d.each(function () {
+        var val = $(this).attr('data-key');
+        var title = $(this).attr('title');
+        //$('#information').append('<div class="col-sm-12 div-detail hide" data-title="' + title + '" data-id="' + i + '" id="' + val + '"></div>');
+        //console.log($(this).attr('data-key'), i, title);
+        i++;
+    });
+    $('#information').append($('#nav'));
+}
+
+/**
+  * @desc set each frames for 360 image
+  * @param 'object' c, which object to store the frame array
+  * @param 'string' source, img file directory 
+*/
+function setFrame(c,source) {
+    c.frames = SpriteSpin.sourceArray(source, {
+        frame: [1, 32],
+        digits: 2
+    })
+}
+
+/**
+  * @desc create navigation for the details.
+*/
+function createNavigation(string) {
+    var a = string;
+    var c = 1;
+
+    for (var i = 0; i < current.detail.length; i++) {
+        var b = '<li><a href="#" onclick="setDetailIndex(' + i + ')">' + c + '</a></li>';
+        a.append(b);
+        c++;
+    }
+}
+/**
+  * @desc set the index for specific frames
+  * @param 'int' index to determine which frame;
+*/
+function setDetailIndex(index) {
+    $('.detail').css({ "display": "none" })
+    current.index = index;
+    if (current.index < 0) {
+        current.index = current.detail.length - 1;
+    }
+    if (current.index >= current.detail.length) {
+        current.index = 0;
+    }
+
+    current.api.playTo(current.detail[current.index]);
+}
+
+/**
+  * @desc preloading immage
+  * @param 'array' arrayOfImages, fill in with array please
+*/
+function preload(arrayOfImages) {
+    $(arrayOfImages).each(function () {
+        $('<img />').attr('src', this).appendTo('body').css('display', 'none');
+    });
+}
+preload(male.frames);
+preload(female.frames);
+/**
+  * @desc preloads everything first, and then run the following code below
+*/
+window.onload = function () {
+    $('#overlay').fadeOut();
+    init();
+    $("#btn-home").click(function () {
+        $('#home').show();
+        $('.div-detail').addClass('hide').removeClass('show');
+    })
+    $("#btn-female").click(function () {
+        female.spin.show();
+        male.spin.hide();
+        current = female;
+    })
+    $("#btn-male").click(function () {
+        male.spin.show();
+        female.spin.hide();
+        current = male;
+    })
+
+}
